475ba7a0a880c7b1410639fc4ab5f77ab949077f
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-04-10 01:06:01 +0530

    Commit Cleanup

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import ConnectionToApi from "./tests/ConnectionToApi.jsx";
 import Header from "./components/Header/Header";
 import DownloadApps from "./components/DownloadApps/DownloadApps";
 import Footer from "./components/Footer/Footer";
+import VehicleDetails from "./components/VehicleDetails/VehicleDetails";
 
 const IS_TESTING = process.env.REACT_APP_TESTING_TRUE || "false";
 
 const VEHICLE_TYPE_ID = new URLSearchParams(window.location.search).get("vehicleTypeId");
 const VEHICLE_ID = new URLSearchParams(window.location.search).get("id");
+
+let isQueryAvailable = false;
+
+if (VEHICLE_TYPE_ID !== null && VEHICLE_ID !== null) isQueryAvailable = true;
 
 // Testing Query String ?vehicleTypeId=25635&id=137332
 const App = () => {
@@ -22,6 +27,7 @@ const App = () => {
 			) : (
 				<div className='App'>
 					<Header />
+					{isQueryAvailable && <VehicleDetails typeId={VEHICLE_TYPE_ID} vehicleId={VEHICLE_ID} />}
 					<DownloadApps />
 					<Footer />
 				</div>
