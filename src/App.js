b7717f6c10cdff1c3943ba73821249b327f7c80f
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2021-04-09 13:12:43 +0530

    Cleanup for basic UI implementation

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import ConnectionToApi from "./tests/ConnectionToApi.jsx";
 
 const IS_TESTING = process.env.REACT_APP_TESTING_TRUE || "false";
 
+const VEHICLE_TYPE_ID = new URLSearchParams(window.location.search).get("vehicleTypeId");
+const VEHICLE_ID = new URLSearchParams(window.location.search).get("id");
+
+// Testing Query String ?vehicleTypeId=25635&id=137332
 const App = () => {
 	return (
 		<>
 			{IS_TESTING === "true" ? (
-				<ConnectionToApi />
+				<div className='App'>
+					<ConnectionToApi />
+				</div>
 			) : (
 				<div className='App'>
 					<h2>Hello World</h2>
+					<br />
+					<div>Type ID: {VEHICLE_TYPE_ID}</div>
+					<div>ID: {VEHICLE_ID}</div>
 				</div>
 			)}
 		</>
